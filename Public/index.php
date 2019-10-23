42180f7fb3dbd3ac20579c2805f658d777a5e480
laupav <l.colomb@laposte.net>
2019-10-23 12:17:41 +0200

    fini3

--- Public/index.php
+++ Public/index.php
@@ -2,7 +2,7 @@
 
 require_once '../vendor/autoload.php';
 
-$products = [ 'Articles', 'Destinations', 'Souvernirs', 'Photos', 'Films', 'Pour finir', 'Le dessert' , 'ouf'];
+$products = [ 'Articles', 'Destinations', 'Souvernirs', 'Photos', 'Films', 'Pour finir', 'Le dessert' , 'ouf', 'enfin'];
 $loader = new \Twig\Loader\FilesystemLoader('../src/views');
 $twig = new \Twig\Environment($loader, ["cache" => false]);
 echo $twig->render('index.html.twig', ['products'=>$products]);
